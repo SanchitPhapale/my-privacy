eb9afd616c5f71b5c3b0736ac57342ea826763ae
Sanchit Phapale <phapalesanchu12@gmail.com>
2026-07-09 21:08:47 +0530

    Fix mobile keyboard issues

--- public/script.js
+++ public/script.js
@@ -318,10 +318,23 @@ messageInput.addEventListener("input", () => {
 });
 
 messageInput.addEventListener("keydown", (e) => {
-    if (e.key === "Enter") {
-        sendMessage();
+
+    if (e.key === "Enter" && !e.shiftKey) {
+
+        e.preventDefault();
+
+        messageInput.setRangeText(
+            "\n",
+            messageInput.selectionStart,
+            messageInput.selectionEnd,
+            "end"
+        );
+
     }
+
 });
+
+
 function adjustTextareaHeight() {
   messageInput.style.height = "auto";
   messageInput.style.height = Math.min(messageInput.scrollHeight, 120) + "px";
